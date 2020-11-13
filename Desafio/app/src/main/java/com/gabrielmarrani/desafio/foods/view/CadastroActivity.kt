65ac8ec8666324f42f382a95d75f9df60b2db117
Marrani <gabrielmarrani@gmail.com>
2020-11-13 11:54:16 -0300

    Validacao cadastro e login

--- Desafio/app/src/main/java/com/gabrielmarrani/desafio/foods/view/CadastroActivity.kt
+++ Desafio/app/src/main/java/com/gabrielmarrani/desafio/foods/view/CadastroActivity.kt
@@ -1,12 +1,55 @@
 package com.gabrielmarrani.desafio.foods.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.gabrielmarrani.desafio.R
+import com.google.android.material.textfield.TextInputEditText
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+
+        findViewById<Button>(R.id.btnRegistrarCadastro).setOnClickListener {
+            val nome = findViewById<TextInputEditText>(R.id.txtNameCad)
+            val email = findViewById<TextInputEditText>(R.id.txtEmailCad)
+            val senha =  findViewById<TextInputEditText>(R.id.txtSenhaCad)
+            val repeatSenha = findViewById<TextInputEditText>(R.id.txtRepeatSenhaCad)
+
+            validarCadastro(nome, email, senha, repeatSenha)
+
+        }
+    }
+
+    private fun validarCadastro(
+        nome: TextInputEditText,
+        email: TextInputEditText,
+        senha: TextInputEditText,
+        repeatSenha: TextInputEditText
+    ) {
+        if (nome.text.toString().isEmpty()) {
+            nome.setError("Favor preencher campo nome")
+        } else if (email.text.toString().isEmpty()) {
+            email.setError("Favor preencher campo e-mail")
+        } else if (senha.text.toString().isEmpty()) {
+            senha.setError("Favor preencher campo senha")
+        } else if (repeatSenha.text.toString().isEmpty()) {
+            repeatSenha.setError("Favor preencher campo repeat senha")
+        } else if (senha.text.toString() != repeatSenha.text.toString()) {
+            repeatSenha.setError("Senha incorreta")
+
+        } else {
+            val toast = Toast.makeText(
+                applicationContext, "Cadastro efetuado!",
+                Toast.LENGTH_LONG
+            ).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
+
--- Desafio/app/src/main/java/com/gabrielmarrani/desafio/foods/view/MainActivity.kt
+++ Desafio/app/src/main/java/com/gabrielmarrani/desafio/foods/view/MainActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.gabrielmarrani.desafio.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +21,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.btnLogin).setOnClickListener {
+            val email = findViewById<TextInputEditText>(R.id.filledTextField)
+            val senha = findViewById<TextInputEditText>(R.id.txtSenha)
+            validarLogin(email, senha)
+
+
+        }
+    }
+
+    private fun validarLogin(
+        email: TextInputEditText,
+        senha: TextInputEditText
+    ) {
+        if (email.text.toString().isEmpty()) {
+            email.setError("Favor preencher campo e-mail")
+        } else if (senha.text.toString().isEmpty()) {
+            senha.setError("Favor preencher campo senha")
+        } else {
             val intent = Intent(this, ListaRestauranteActivity::class.java)
             startActivity(intent)
         }
